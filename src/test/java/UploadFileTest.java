59a62553b02d5d1f4f6e26cb0fc18ed3a4f39aab
Sergey <smartserega85@gmail.com>
2018-02-28 19:44:07 +0300

    little code refactoring

--- src/test/java/UploadFileTest.java
+++ src/test/java/UploadFileTest.java
@@ -21,31 +21,29 @@ public class UploadFileTest {
         System.out.println("Открылся сайт почты mail.ru");
     }
     @Test
-            public void userLogin() throws InterruptedException {
+    public void userLogin() {
+        WebElement loginField = driver.findElement(By.name("login"));
+        loginField.sendKeys("smarseregatest");
+        System.out.println("Пользователь ввёл логин");
+
+        WebElement passwordField = driver.findElement(By.name("password"));
+        passwordField.sendKeys("12345678");
+        System.out.println("Пользователь ввёл пароль");
+
+        WebElement loginButton = driver.findElement(By.cssSelector(".form-button.form-button_submit"));
+        loginButton.click();
+        System.out.println("Пользователь вошёл в почту");
+
+        WebElement writeButton = driver.findElement(By.className("_Button-label-3x"));
+        writeButton.click();
+        System.out.println("Пользователь начал писать новое письмо");
+
+        //нужно выибрать селектор с тегом input
+        driver.findElement(By.className("Compose-fileInput-dl")).sendKeys("C:\\Users\\ПК\\IdeaProjects\\AutomatingTestingJavaMaven\\src\\recourses\\chromedriver.exe");
 
 
-                WebElement loginField = driver.findElement(By.name("login"));
-                loginField.sendKeys("smarseregatest");
-                System.out.println("Пользователь ввёл логин");
-
-                WebElement passwordField = driver.findElement(By.name("password"));
-                passwordField.sendKeys("12345678");
-                System.out.println("Пользователь ввёл пароль");
-
-                WebElement loginButton = driver.findElement(By.cssSelector(".form-button.form-button_submit"));
-                loginButton.click();
-                System.out.println("Пользователь вошёл в почту");
-
-                WebElement writeButton = driver.findElement(By.className("_Button-label-3x"));
-                writeButton.click();
-                System.out.println("Пользователь начал писать новое письмо");
-
-                //нужно выибрать селектор с тегом input
-                driver.findElement(By.className("Compose-fileInput-dl")).sendKeys("C:\\Users\\ПК\\IdeaProjects\\AutomatingTestingJavaMaven\\src\\recourses\\chromedriver.exe");
-
-
-                WebElement check = driver.findElement(By.className("Uploads-name-2k"));
-                System.out.println("Пользователь загружает файл");
+        WebElement check = driver.findElement(By.className("Uploads-name-2k"));
+        System.out.println("Пользователь загружает файл");
     }
     @AfterClass
         public static void tearDown() {
